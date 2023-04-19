4aaeeec4b7601d80e06026aeff72936a127d6e9f
Matthew Watkkins <mattwat1@gmail.com>
2023-04-19 21:45:15 +0200

    initial commit

---
+++ resources/components/HelloVue.vue
@@ -0,0 +1,12 @@
+// resources/components/HelloVue.vue
+
+<template>
+  <h1>Hello Vue!123</h1>
+  <p>This is a simple block of text following the header</p>
+</template>
+
+<script>
+export default {
+    name: 'HelloVue'
+}
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import HelloVue from '../components/HelloVue.vue';
+
+createApp({
+    components: {
+        HelloVue,
+    }
+}).mount('#app');
